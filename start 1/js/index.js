9c4d42f27253910c5053a1a69974452c2242071c
ManuelSenatore <106093444+ManuelSenatore@users.noreply.github.com>
2022-06-08 16:37:18 +0200

    Add files via upload

---
+++ start 1/js/index.js
@@ -0,0 +1,37 @@
+var a =('Manuel')
+console.log(a);
+let b = ('Sta studiando');
+console.log(b);
+const c = ('javascript');
+console.log(c);
+var d =('Manuel');
+var d = ('Se lo riscrivo cambia');
+console.log(d);
+let e =('posso riscriverlo solo all\'interno di uno scope');
+console.log(e);
+var f =(5*5);
+console.log(f);
+let g =(undefined);
+console.log(g)
+let h =(5 + 8 * 6)
+console.log(h)
+var i =('bici');
+i += 'cletta';
+console.log(i)
+var l = 
+`posso anche scrivere
+su più
+stringhe`
+console.log(l)
+var m =('io mi occupo di:');
+var n =('developing e formazione');
+console.log(m + n);
+var x = 5;
+var y = 10;
+var z = x + y;
+alert(z);
+var firstName = "Manuel"; lastName = "Senatore"; age = 25;
+console.log(firstName + lastName + age);
+
+
+
